bc820ecbc3368e1a2fc143744f5ee316d143bdf3
Anne-Marie <annemarierommerdahl@gmail.com>
2022-10-25 14:02:48 +0200

    Set up repo for testing

--- GitInsight.Tests/UnitTest1.cs
+++ GitInsight.Tests/UnitTest1.cs
@@ -1,11 +1,35 @@
 namespace GitInsight.Tests;
+using GitInsight.Program;
+//using (var repo = new Repository(@"/Users/sushi/code/sushi/Xamarin.Forms.Renderer.Tests"))
+
+
+//søndag d.25 setember - 2 commits
+//Torsdag d.6 octover - 6 commits
+//Fredag d.7 october - 3 commits
+//Lørdag d.8 october - 10 commits
+//Søndag d.9 octover - 1 commits
+//Torsdag d.13 octover - 3 commits
 
 public class UnitTest1
 {
     [Fact]
     public void GetListOfAllCommits()
     {
-        
+        //Arrange
+        var repoPath = @"C:\Users\whoever\Documents\git\SomeRepoName";
+        var fileOffset = @"YouTube\subscriptions\subscriptions.json";
+        var fileOffsetFwdSlash = fileOffset.Replace("\\", "/");
+        using (var repo = new Repository(repoPath))
+        {
+            var logs = repo.Commits.QueryBy(fileOffsetFwdSlash).ToList();
+            foreach (var log in logs)
+            {
+                Debug.WriteLine(log.Commit.Author.When);
+            }
+        }
+        //Act
+
+        //Actual
 
     }
 }
--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GitInsigt;
+using LibGit2Sharp;
+
+public class GitInsight{
+
+    public static void Main(string[] args){
+        commitFrequencyMode();
+    }
+    
+    public void commitAuthorMode(){
+        throw new NotImplementedException();
+    }
+
+    public static void commitFrequencyMode(){
+        var repoPath = @"C:\Users\annem\Desktop\BDSA_PROJECT\GitInsight.Tests\assignment-05";
+        var fileOffset = @"C:\Users\annem\Desktop\BDSA_PROJECT\GitInsight.Tests\assignment-05\GildedRose\obj\project.assets.json";
+        var fileOffsetFwdSlash = fileOffset.Replace("\\", "/");
+        using (var repo = new Repository(repoPath))
+        {
+            var loges = repo.Commits.ToList();
+            Console.WriteLine(loges.Count);
+            
+            var dates = loges.GroupBy(x => x.Author.When.Date).Count();//.SelectMany(x=>x).ToList();
+            Console.WriteLine(dates);
+            /*foreach (var date in dates){
+                //Console.WriteLine(date.Take(2));
+                foreach (var stuff in date){
+                    Console.WriteLine(stuff);
+                } 
+            }*/
+            
+            //foreach (var log in loges){
+              //  Console.WriteLine(log + " " + log.Author.When.Date);
+            //}
+        }
+    }
+    
+}
