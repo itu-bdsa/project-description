d4f7b6dcd2793f0c02520317d384ad8f2107db0c
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2022-10-28 13:17:08 +0100

    ..

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace GitInsigt;
+﻿using System.Linq.Expressions;
+using System.Collections;
+namespace GitInsigt;
 using LibGit2Sharp;
 
 public class GitInsight{
 
     public static void Main(string[] args){
-        commitFrequencyMode();
+        //commitFrequencyMode();
+        commitUserFrequencyMode();
     }
     
     public void commitAuthorMode(){
@@ -17,20 +20,38 @@ public class GitInsight{
         var fileOffsetFwdSlash = fileOffset.Replace("\\", "/");
         using (var repo = new Repository(repoPath))
         {
-            var loges = repo.Commits.ToList();
-    Console.WriteLine(loges.Count);
-            for (int i = 0; i < loges.Count; i++)
+            var commitArray = repo.Commits.ToList();
+            Console.WriteLine(commitArray.Count);
+            ArrayList dateArray = new ArrayList();
+            for (int i = 0; i < commitArray.Count; i++)
             {
-        
-            string[] uwu = loges[i].Author.ToString().Split(" ");
-            Console.WriteLine(uwu[0]);
-            string[] owo = loges[i].Author.When.ToString().Split(" ");
-            Console.WriteLine(owo[0]);
+            string[] tempDateArray = commitArray[i].Author.When.ToString().Split(" ");
+            dateArray.Add(tempDateArray[0]);
             }
 
+            dateArray.Sort();
+            foreach (var item in dateArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(dateArray.Count);
+            var currentDate = dateArray[0];
+            var currentDateCount = 0;
+            foreach (var item in dateArray)
+            {
 
-            var dates = loges.GroupBy(x => x.Author.When.Date).Count();//.SelectMany(x=>x).ToList();
-            Console.WriteLine(dates);
+                if(item.Equals(currentDate)){
+                    currentDateCount = currentDateCount+1;
+                } else {
+                    Console.WriteLine(currentDateCount + " " + currentDate.ToString());
+                    currentDate = item.ToString();
+                    currentDateCount = 1;
+                }
+
+            }
+            Console.WriteLine(currentDateCount + " " + currentDate.ToString());
+            //var dates = loges.GroupBy(x => x.Author.When.Date).Count();//.SelectMany(x=>x).ToList();
+            //Console.WriteLine(dates);
             /*foreach (var date in dates){
                 //Console.WriteLine(date.Take(2));
                 foreach (var stuff in date){
@@ -43,5 +64,72 @@ public class GitInsight{
             //}
         }
     }
+
+    public static void commitUserFrequencyMode(){
+        var repoPath = @"C:\Users\Bruger\Desktop\BDSA Projekt\BDSA_PROJECT\GitInsight.Tests\Assignment5\assignment-05";
+        var fileOffset = @"C:\Users\annem\Desktop\BDSA_PROJECT\GitInsight.Tests\assignment-05\GildedRose\obj\project.assets.json";
+        var fileOffsetFwdSlash = fileOffset.Replace("\\", "/");
+        using (var repo = new Repository(repoPath))
+        {
+            var commitArray = repo.Commits.ToList();
+            Console.WriteLine(commitArray.Count);
+            var dateAuthorArray = new List<List<String>>();
+
+
+            for (int i = 0; i < commitArray.Count; i++)
+            {
+            string[] tempDateArray = commitArray[i].Author.ToString().Split(" ");
+            
+            bool containsInList = false;
+
+            foreach (var item in dateAuthorArray)
+            {
+                if (item[0].Equals(tempDateArray[0])){
+                    containsInList = true;
+                } 
+            }
+
+            if(!containsInList){
+            var AuthorList = new List<String>();
+            AuthorList.Add(tempDateArray[0]);
+            dateAuthorArray.Add(AuthorList);
+            }
+            }
+
+
+            for (int i = 0; i < commitArray.Count; i++)
+            {
+            string[] tempDateArray = commitArray[i].Author.When.ToString().Split(" ");
+            dateArray.Add(tempDateArray[0]);
+            }
+
+
+
+            dateArray.Sort();
+            foreach (var item in dateArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(dateArray.Count);
+            var currentDate = dateArray[0];
+            var currentDateCount = 0;
+            foreach (var item in dateArray)
+            {
+
+                if(item.Equals(currentDate)){
+                    currentDateCount = currentDateCount+1;
+                } else {
+                    Console.WriteLine(currentDateCount + " " + currentDate.ToString());
+                    currentDate = item.ToString();
+                    currentDateCount = 1;
+                }
+
+            }
+            Console.WriteLine(currentDateCount + " " + currentDate.ToString());
+
+
+        }
+    }
+ 
     
 }
