715a29173754a0034bb2068896a7ddecb05b680d
thekure <laku@itu.dk>
2022-11-04 14:35:37 +0100

    Update requirements, create Class Diagram, w44
    Co-authored-by: megsikon <megsikon@users.noreply.github.com>

--- code/GitInsight/Control.cs
+++ code/GitInsight/Control.cs
@@ -32,7 +32,7 @@ class Control
                     }
                     else
                     {
-                        _repo!.print(_mode);
+                        _repo!.Print(_mode);
                     }
                     break;
                 case "newpath":
@@ -51,7 +51,7 @@ class Control
                     Console.WriteLine("'exit' to exit program");
                     break;
                 case "exit":
-                    _repo!.shutDown();
+                    _repo!.Shutdown();
                     isRunning = false;
                     break;
                 default:
