9e2a6c26614189ae611202fb31dd95ef28ee15fb
Smus4 <raoo@itu.dk>
2022-11-08 16:26:35 +0100

    DOne with feature/DB branch

--- code/GitInsight/CommitTreeContextFactory.cs
+++ code/GitInsight/CommitTreeContextFactory.cs
@@ -1,19 +1,19 @@
-// namespace GitInsight;
+namespace GitInsight;
 
-// public class CommitTreeContextFactory : IDesignTimeDbContextFactory<CommitTreeContext>
-// {
+public class CommitTreeContextFactory : IDesignTimeDbContextFactory<CommitTreeContext>
+{
 
-//     public CommitTreeContextFactory CreateDbContext(string[] args)
-//     {
-//         var configuration = new ConfigurationBuilder().AddUserSecrets<CommitTreeContext>().Build();
-//         var connectionString = configuration.GetConnectionString("ConnectionString");
+    public CommitTreeContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder().AddUserSecrets<CommitTreeContext>().Build();
+        var connectionString = configuration.GetConnectionString("ConnectionString");
 
-//         String connectionStringHardcode = "Server=localhost;Database=stoic_chandrasekhar;User Id=sa;Password=BDSAagain22;Trusted_Connection=False;Encrypt=False";
+        String connectionStringHardcode = "Server=localhost;Database=stoic_chandrasekhar;User Id=sa;Password=BDSAagain22;Trusted_Connection=False;Encrypt=False";
 
-//         var optionsBuilder = new DbContextOptionsBuilder<CommitTreeContext>();
-//         optionsBuilder.UseSqlServer(connectionStringHardcode);
+        var optionsBuilder = new DbContextOptionsBuilder<CommitTreeContext>();
+        optionsBuilder.UseSqlServer(connectionStringHardcode);
 
-//         return new CommitTreeContext(optionsBuilder.Options);
-//     }
+        return new CommitTreeContext(optionsBuilder.Options);
+    }
 
-// }
+}
--- code/GitInsight/Usings.cs
+++ code/GitInsight/Usings.cs
@@ -2,6 +2,6 @@ global using LibGit2Sharp;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.EntityFrameworkCore;
-// global using GitInsight.Entities;
+global using GitInsight.Entities;
 
 
