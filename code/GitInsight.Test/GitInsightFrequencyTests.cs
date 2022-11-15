bf6a8dc4d80c505e99f2573300efb3dcaae008c6
Benjamin Millant <bemi@itu.dk>
2022-11-15 12:52:59 +0100

    .

--- code/GitInsight.Test/GitInsightFrequencyTests.cs
+++ code/GitInsight.Test/GitInsightFrequencyTests.cs
@@ -21,10 +21,10 @@ public class GitInsightFrequencyTests : IDisposable
 
         //24/08/2008, 3
         context.Frequencies.AddRange(
-            new Frequency(DateTime.Parse("24/08/2008")),
-            new Frequency(DateTime.Parse("20/10/2009")),
-            new Frequency(DateTime.Parse("24/08/2010")),
-            new Frequency(DateTime.Parse("04/03/2011"))
+            new Frequency(System.DateTime.Parse("24/08/2008")),
+            new Frequency(System.DateTime.Parse("20/10/2009")),
+            new Frequency(System.DateTime.Parse("24/08/2010")),
+            new Frequency(System.DateTime.Parse("04/03/2011"))
         );
         context.SaveChanges();
 
