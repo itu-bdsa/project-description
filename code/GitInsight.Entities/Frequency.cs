04a8e80433da72473e57fafa0f6161e8b18bb405
Benjamin Millant <bemi@itu.dk>
2022-11-15 12:09:23 +0100

    Co-authored-by: megsikon <megsikon@users.noreply.github.com> Co-authored-by: thekure <thekure@users.noreply.github.com>

--- code/GitInsight.Entities/Frequency.cs
+++ code/GitInsight.Entities/Frequency.cs
@@ -1,17 +1,18 @@
 namespace GitInsight.Entities;
 
-public class Frequency {
+public class Frequency
+{
 
-    public Frequency (DateTime Date) {
+    public Frequency(int id, DateTime Date)
+    {
         this.Date = Date;
     }
 
 
-    public DateTime Date {get;}
+    public DateTime Date { get; set; }
 
-    public int Count {get; set;}
+    public int Count { get; set; }
 
     [Key]
-    public int Id {get;}
-
+    public int Id { get; set; }
 }
--- code/GitInsight.Entities/FrequencyRepository.cs
+++ code/GitInsight.Entities/FrequencyRepository.cs
@@ -13,20 +13,29 @@ public class FrequencyRepository : IFrequencyRepository
         this.context = context;
     }
 
-    public void Create (FrequencyCreateDTO frequency) {
+    public void Create(FrequencyCreateDTO frequency)
+    {
 
-        var entry = new Frequency(frequency.Date);
+        var entry = new Frequency(1, frequency.Date);
         context.Frequencies.Add(entry);
         context.SaveChanges();
 
         Console.WriteLine(frequency.Date + " has been created");
     }
 
-    public IReadOnlyCollection<FrequencyDTO> ReadAll(){
-        return context.Frequencies.Select ( e => new FrequencyDTO(e.Id)).ToList().AsReadOnly();
+    public IReadOnlyCollection<FrequencyDTO> ReadAll()
+    {
+        return context.Frequencies.Select(e => new FrequencyDTO(e.Id)).ToList().AsReadOnly();
     }
 
-    public void Update(){
+    public void Update()
+    {
+        // not implemented yet
+        // context.Frequenices.delete
+    }
+
+    public void Delete(int Id)
+    {
         // not implemented yet
         // context.Frequenices.delete
     }
