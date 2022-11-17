ff4a018880f98c0a114880e37b106b86af1b268a
thekure <laku@itu.dk>
2022-11-17 16:22:19 +0100

    everything is different but still the same

--- code/View/Program.cs
+++ code/View/Program.cs
@@ -9,11 +9,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => 
     new HttpClient{
-        BaseAddress = new Uri("https://localhost:7221")
-    });
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 builder.Services.AddHttpClient("WebAPI", client => 
-    client.BaseAddress = new Uri("https://localhost:7221")
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 );
             
 await builder.Build().RunAsync();
