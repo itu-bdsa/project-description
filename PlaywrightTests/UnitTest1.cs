43e62984d83896d28df7c3c6074f4c46570b8775
eikbo <eikb@itu.dk>
2022-11-18 12:01:26 +0100

    This semi works

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -1,8 +1,8 @@
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using GitInsight;
 using NUnit.Framework;
 
 namespace PlaywrightTests;
@@ -14,11 +14,26 @@ public class Tests : PageTest
 
     //Startup Website, pgAdmin needs to be turned on
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task WeCanNavigateToFetchDataAndYoinkSomeData()
     {
-        await Page.GotoAsync("https://localhost:7111/fetchdata");
-        await Expect(Page).ToHaveTitleAsync("GitInsight");
-                //await Page.Locator("input type").TypeAsync("SpaceVikingEik/hewwo"); does not work
+
+         await Page.GotoAsync("https://localhost:7111/");
+
+        await Page.GetByRole(AriaRole.Link, new() { NameString = "Fetch data" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Textbox).First.ClickAsync();
+
+        await Page.GetByRole(AriaRole.Textbox).First.FillAsync("SpaceVikingEik/hewwo");
+
+        await Page.GetByRole(AriaRole.Textbox).First.PressAsync("Tab");
+
+        await Page.GetByRole(AriaRole.Textbox).Nth(1).FillAsync("FQMode");
+
+        await Page.GetByRole(AriaRole.Button, new() { NameString = "Run Analysis" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Row, new() { NameString = "SpaceVikingEik/hewwo 12-11-2022 4" }).GetByRole(AriaRole.Cell, new() { NameString = "SpaceVikingEik/hewwo" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Row, new() { NameString = "SpaceVikingEik/hewwo 13-11-2022 4" }).GetByRole(AriaRole.Cell, new() { NameString = "SpaceVikingEik/hewwo" }).ClickAsync();
 
     }
 }
