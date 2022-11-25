55179591f8cd5c7adade00fbb4c3e502599a8184
Anne-Marie Roommerdahl <annemarierommerdahl@gmail.com>
2022-11-25 12:09:35 +0100

    auto

---
+++ GitInsight.Entities/GitFileAnalyzer.cs
@@ -0,0 +1,48 @@
+namespace GitInsight.Entities;
+using LibGit2Sharp;
+
+public class GitFileAnalyzer{
+
+    public record FileAndNrChanges(string fileName, int changes);
+
+    public List<FileAndNrChanges> getFilesAndNrChanges(Repository repo){
+        //sort commits by date
+        var filter = new CommitFilter {
+        SortBy = CommitSortStrategies.Time | CommitSortStrategies.Reverse,
+        };
+        var commits = repo.Commits.QueryBy(filter);
+
+        //Find changes between trees, save changes to list
+        var tempList = new List<TreeEntryChanges>();
+        for (int i = 0; i < commits.Count(); i++){
+            if(i+1 < commits.Count()){
+                var temp = repo.Diff.Compare<TreeChanges>(commits.ElementAt(i).Tree, commits.ElementAt(i + 1).Tree);
+
+                foreach(var change in temp){
+                    tempList.Add(change);
+                }
+            }
+        }
+
+        var returnList = new List<FileAndNrChanges>();
+        //select path, Count(path) ... group by path
+        var r = tempList.GroupBy(c => c.Path).Select(s => new { path = s.Key, count = s.Count()});
+        foreach(var p in r){
+            Console.WriteLine("path: " + p.path + " count: " + p.count);
+            returnList.Add(new FileAndNrChanges(p.path, p.count));
+        }
+
+        return returnList;
+
+    }
+
+    public FileAndNrChanges getMostFreqChangedFile(List<FileAndNrChanges> fileList){
+        //find path/file that has been changed most frequently
+        //there can be multiple, but we only take the first one found
+        var mostFreqChangedFile = fileList.Where(c => c.changes == 
+                                (fileList.Select(k => k.changes).Max()))
+                                .First();
+
+        return mostFreqChangedFile;
+    }
+}
---
+++ GitInsight.Tests/GitAnalysisTest.cs
@@ -0,0 +1,48 @@
+namespace GitInsight.Tests;
+using GitInsight.Entities;
+public class GitAnalysisTest{
+    Repository _repo;
+    public GitAnalysisTest(){
+        var l = Directory.GetParent(Environment.CurrentDirectory)!.ToString();
+        string path = Path.Combine(l, @"TestGithubStorage/VictoriousAnnro-TestRep.git");
+        if(!Directory.Exists(path)){
+            //Console.WriteLine("DOESNT exist!");
+            var clonePath = Repository.Clone("https://github.com/VictoriousAnnro/TestRep.git", path);
+        }
+        _repo = new Repository(path);
+        Commands.Pull(_repo,new Signature(" d", "d ",new DateTimeOffset()),new PullOptions());
+    }
+
+    [Fact]
+    public void ListOfChangesShouldBeProgram2_Tester1(){
+        
+        //Arrange
+        var analyser = new GitFileAnalyzer();
+
+        //Act
+        var expected = new List<GitFileAnalyzer.FileAndNrChanges>();
+        expected.Add(new GitFileAnalyzer.FileAndNrChanges("Program.cs", 2));
+        expected.Add(new GitFileAnalyzer.FileAndNrChanges("Tester.cs", 1));
+        
+        var actual = analyser.getFilesAndNrChanges(_repo);
+
+        //Assert
+        actual.Equals(expected);
+    }
+
+
+    [Fact]
+    public void MostFreqChangedFileProgramcsW2Changes(){
+        
+        //Arrange
+        var analyser = new GitFileAnalyzer();
+        var list = analyser.getFilesAndNrChanges(_repo);
+
+        //Act
+        var expected = new GitFileAnalyzer.FileAndNrChanges("Program.cs", 2);
+        var actual = analyser.getMostFreqChangedFile(list);
+
+        //Assert
+        actual.Should().Be(expected);
+    }
+}
